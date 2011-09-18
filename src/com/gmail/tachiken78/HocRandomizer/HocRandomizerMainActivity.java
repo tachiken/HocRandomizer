0895592c98f9d55508af27612540c7e6f02c2d51
Tachiken <tachiken78@gmail.com>
2011-09-18 13:20:01 +0900

    Create a new android application project.

---
+++ src/com/gmail/tachiken78/HocRandomizer/HocRandomizerMainActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.tachiken78.HocRandomizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HocRandomizerMainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
